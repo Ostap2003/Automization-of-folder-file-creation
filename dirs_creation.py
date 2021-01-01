e6a9fbd20ac841ed6752fee5d40548019415a2b0
Ostap <ostap.dutka@ucu.edu.ua>
2021-01-01 14:10:17 +0200

    New version

--- dirs_creation.py
+++ dirs_creation.py
@@ -24,25 +24,3 @@ for i in range(len(usr_dirs)):
     if usr_fold[i] != '':
         with open(usr_fold[i], 'w') as empt_fl:
             pass
-
-
-# usr_dir = input('/home/ostap/Documents/python_projects/\n')
-# if usr_dir != '':
-#     os.chdir(usr_dir)
-
-# # here user can skip
-# create_dir = input('Enter what dirs you want to create\n')
-
-# # if user don't want to create folder, then don't make dirs
-# if create_dir != '':
-#     os.makedirs(create_dir)
-#     os.chdir(create_dir)
-
-
-# while True:
-#     file_name = input('Enter filename\n')
-#     if file_name != '':
-#         with open(file_name, 'w') as empt_file:
-#             pass
-#     else:
-#         break
